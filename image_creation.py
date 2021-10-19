9896d814f05537ae22e66184c22ac4dda2929383
Johan Ketkar <johanketkar@johans-mbp.lan>
2021-10-18 21:16:28 -0500

    APPL 1005 Q1

--- image_creation.py
+++ image_creation.py
@@ -5,9 +5,29 @@ from PIL import Image
 
 
 
-q1_df = pd.read_csv('/Users/johanketkar/Projects/stock-valuation-project/data/SF1/AAPL/2005/AAPL_Q1_2005.csv')
-y_df = pd.read_csv('/Users/johanketkar/Projects/stock-valuation-project/data/SF1/AAPL/2005/AAPL_fy_2005.csv')
+q1_df = pd.read_csv('/Users/johanketkar/Projects/stock-valuation-project/data/SF1/AAPL/2005/AAPL_Q1_2005.csv').fillna(0)
+y_df = pd.read_csv('/Users/johanketkar/Projects/stock-valuation-project/data/SF1/AAPL/2005/AAPL_fy_2005.csv').fillna(0)
 
-print(q1_df)
+mrq = q1_df.loc[q1_df['dimension'] == 'MRQ'].to_numpy()[0]
+mrt = q1_df.loc[q1_df['dimension'] == 'MRT'].to_numpy()[0]
+mry = y_df.loc[y_df['dimension'] == 'MRY'].to_numpy()[0]
 
-print(y_df)
+mrq = mrq[7:]
+mrt = mrt[7:]
+mry = mry[7:]
+
+mrq = mrq.reshape(15,7)
+mrt = mrt.reshape(15,7)
+zeros = np.zeros((15,7), 'uint8')
+print(mrq)
+print(len(mrt))
+print(len(mry))
+
+rgb = np.dstack((mrq,mrt, zeros)).astype('uint8')
+
+
+img = Image.fromarray(rgb, mode='RGB')
+
+img.show()
+
+
