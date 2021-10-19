2b9a1cf6c5671e664f2e84821115af7be731c0b6
Johan Ketkar <johanketkar@johans-mbp.lan>
2021-10-18 20:37:38 -0500

    image create initial commit

---
+++ image_creation.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from PIL import Image
+
+
+
+
+q1_df = pd.read_csv('/Users/johanketkar/Projects/stock-valuation-project/data/SF1/AAPL/2005/AAPL_Q1_2005.csv')
+y_df = pd.read_csv('/Users/johanketkar/Projects/stock-valuation-project/data/SF1/AAPL/2005/AAPL_fy_2005.csv')
+
+print(q1_df)
+
+print(y_df)
