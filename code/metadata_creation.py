e8892d7052b97aa10b9dd2efb5e71d7a53238545
Johan Ketkar <johanketkar@johans-mbp.lan>
2021-10-24 20:29:36 -0500

    get ticker list and loop through

--- code/metadata_creation.py
+++ code/metadata_creation.py
@@ -1,14 +1,22 @@
 import constants
+import quandl
+import pandas as pd
+import os
 
-print(constants.CODE_PATH)
+df = quandl.get_table("SHARADAR/INDICATORS", table='SF1')
+indicator_list = df['indicator'].tolist()
+need_to_remove = ['ticker', 'dimension', 'calendardate', 'datekey', 'reportperiod', 'lastupdated']
 
-df = quandl.get_table("SHARADAR/SF1", ticker='AAPL', calendardate={'gte': '2019-01-01', 'lte': '2020-12-31'})
+for x in need_to_remove:
+    indicator_list.remove(x)
 
-print(df)
+indicator_list.sort()
+
 
 #get list of all indicators (columns)
 
 #get list of all quarter report dates (rows for quarterly_coverage)
+
 
 #get list of all years (rows for yearly_coverage)
 
@@ -22,3 +30,12 @@ print(df)
         #store dimension and calendardate
         #for every item in row
             #if item is not nan then update corresponding coverage dataframe item to 1
+sp500 = pd.read_csv(os.path.join(constants.DATA_PATH,'SP500_all_constituents.csv'))
+sp500.columns = ["index", "ticker"]
+sp500.pop("index")
+
+
+for t in sp500['ticker']:
+    file_name = 'SF1/'+t+'/'+t+'_mr.csv'
+    mr_df = pd.read_csv(os.path.join(constants.DATA_PATH, file_name))
+    
