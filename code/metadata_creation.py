a413fb12735bab8b7243ff1ba315f8971ba924fd
Johan Ketkar <johanketkar@johans-mbp.lan>
2021-10-30 16:38:09 -0500

    metadata

--- code/metadata_creation.py
+++ code/metadata_creation.py
@@ -1,41 +1,77 @@
 import constants
-import quandl
 import pandas as pd
 import os
 
-df = quandl.get_table("SHARADAR/INDICATORS", table='SF1')
-indicator_list = df['indicator'].tolist()
-need_to_remove = ['ticker', 'dimension', 'calendardate', 'datekey', 'reportperiod', 'lastupdated']
+indicator_total_null_dict ={}
+total_reports = 0
+for indicator in constants.SF1_INDICATORS:
+    indicator_total_null_dict[indicator] = 0
 
-for x in need_to_remove:
-    indicator_list.remove(x)
+quarter_report_dates = []
+yearly_report_dates = []
+for year in constants.ALL_YEARS:
+    q1 = str(year)+ '-' + constants.Q1_REPORT_MONTH_DAY_STRING
+    q2 = str(year)+ '-' +constants.Q2_REPORT_MONTH_DAY_STRING
+    q3 = str(year)+ '-' +constants.Q3_REPORT_MONTH_DAY_STRING
+    q4 = str(year)+ '-' +constants.Q4_REPORT_MONTH_DAY_STRING
+    yearly = str(year)+ '-' +constants.ANNUAL_REPORT_MONTH_DAY_STRING
+    quarter_report_dates.append(q1)
+    quarter_report_dates.append(q2)
+    quarter_report_dates.append(q3)
+    quarter_report_dates.append(q4)
+    yearly_report_dates.append(yearly)
 
-indicator_list.sort()
+for t in constants.SP500_TICKERS:
+    mrq_coverage = {}
+    mrq_coverage['dimension'] = 'MRQ'
+    mry_coverage = {}
+    mry_coverage['dimension'] = 'MRY'
+    mrt_coverage = {}
+    mrt_coverage['dimension'] = 'MRT'
+
+    mr_file_name = 'SF1/'+t+'/'+t+'_mr.csv'
+    coverage_file_name = 'SF1/'+t+'/'+t+'_coverage.csv'
+    try:
+        mr_df = pd.read_csv(os.path.join(constants.DATA_PATH, mr_file_name))
+    except FileNotFoundError:
+        continue
+
+    for indicator in constants.SF1_INDICATORS:
+        indicator_total_null_dict[indicator] += mr_df[indicator].isna().sum()
+
+    for date in quarter_report_dates:
+        if ((mr_df['dimension'] == 'MRQ') & (mr_df['calendardate'] == date)).any():
+            total_reports += 1
+            mrq_coverage[date] = 1
+        else:
+            mrq_coverage[date] = 0
+        
+        if ((mr_df['dimension'] == 'MRY') & (mr_df['calendardate'] == date)).any():
+            total_reports += 1
+            mry_coverage[date] = 1
+        else:
+            mry_coverage[date] = 0
+        
+        if ((mr_df['dimension'] == 'MRT') & (mr_df['calendardate'] == date)).any():
+            total_reports += 1
+            mrt_coverage[date] = 1
+        else:
+            mrt_coverage[date] = 0
+
+    mrq_coverage_df = pd.DataFrame(mrq_coverage, index = [0])
+    mry_coverage_df = pd.DataFrame(mry_coverage, index = [0])
+    mrt_coverage_df = pd.DataFrame(mrt_coverage, index = [0])
+ 
+    coverage_df = mrq_coverage_df.append(mry_coverage_df).append(mrt_coverage_df)
+    #coverage_df.to_csv(os.path.join(constants.DATA_PATH, coverage_file_name))
+
+print(indicator_total_null_dict)
 
 
-#get list of all indicators (columns)
-
-#get list of all quarter report dates (rows for quarterly_coverage)
+    
 
 
-#get list of all years (rows for yearly_coverage)
-
-#create three dataframes of zeros
-    #quarterly_coverage
-    #yearly_coverage
-    #trailing_coverage
-
-#loop through every ticker and correspodning mr csv 
-    #for every row 
-        #store dimension and calendardate
-        #for every item in row
-            #if item is not nan then update corresponding coverage dataframe item to 1
-sp500 = pd.read_csv(os.path.join(constants.DATA_PATH,'SP500_all_constituents.csv'))
-sp500.columns = ["index", "ticker"]
-sp500.pop("index")
 
 
-for t in sp500['ticker']:
-    file_name = 'SF1/'+t+'/'+t+'_mr.csv'
-    mr_df = pd.read_csv(os.path.join(constants.DATA_PATH, file_name))
+
     
