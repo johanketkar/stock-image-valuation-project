e5fa35fb588edfe7872caa08b0fc80f130b8f1ea
Johan Ketkar <johanketkar@johans-mbp.lan>
2021-12-01 15:53:23 -0600

    per cat acc with counts for predictions

--- code/model_test.py
+++ code/model_test.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import classification_report
+import numpy as np
 
 
 model = keras.models.load_model(constants.PROJECT_PATH+'/mymodel')
@@ -29,7 +30,33 @@ y_test = test_data_gen.labels
 
 y_pred = model.predict(test_data_gen, verbose=1)
 
-print(classification_report(y_test, y_pred))
+predictions = np.argmax(y_pred, axis = 1)
+#print(predictions)
+count_0 = 0
+count_1 = 0
+count_2 = 0
+count_3 = 0
+count_4 = 0
+for cat in predictions:
+    if cat == 0:
+        count_0+=1
+    elif cat == 1:
+        count_1+=1
+    elif cat == 2:
+        count_2+=1
+    elif cat == 3:
+        count_3+=1
+    elif cat == 4:
+        count_4+=1
+
+print("Count 0", count_0)
+print("Count 1", count_1)
+print("Count 2", count_2)
+print("Count 3", count_3)
+print("Count 4", count_4)
+     
+
+print(classification_report(y_test, predictions))
 
 
 
